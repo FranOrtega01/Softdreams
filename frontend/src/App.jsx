ccf27ac4ca4ea29a4b8a2f5ede912e201b68bf65
FranOrtega01 <franortega.wg@gmail.com>
2024-09-20 19:22:37 -0300

    SoftDreams

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Header } from "./Components/Header/Header";
+import { ItemDetailContainer } from "./Components/ItemDetailContainer/ItemDetailContainer";
+import { Home } from "./Screens/Home";
+import { AboutUs } from './Screens/AboutUs';
+import { Contact } from './Screens/Contact';
+import { Footer } from './Components/Footer/Footer.jsx';
+import { Admin } from "./Screens/Admin/Admin.jsx";
+import { Login } from './Screens/Login.jsx';
+import { AuthProvider } from './Context/AuthContext.jsx';
+import { RedirectIconContainer as RIconContainer } from './Components/RedirectIcon/RedirectIconContainer.jsx';
+import logo from './assets/logo2.png';
+import { Navbar } from './Components/Header/Navbar.jsx';
+import { PrivateRoute } from './Components/PrivateRoute/PrivateRoute.jsx';
+import ScrollToTop from './Components/ScrollToTop.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <AuthProvider>
+            <BrowserRouter>
+                <ScrollToTop />
+                <Routes>
+                    <Route
+                        path="/admin"
+                        element={<PrivateRoute
+                            to={'/login'}><Admin />
+                        </PrivateRoute>}
+                    />
+                    <Route path="/login" element={<Login />} />
+                    <Route
+                        path="/*"
+                        element={
+                            <>
+                                <Header className='grid'>
+                                    <Link to={'/'} className='logo'>
+                                        <img src={logo} alt="SoftDreams" />
+                                    </Link>
+                                    <Navbar />
+                                </Header>
+                                <main>
+                                    <RIconContainer />
+                                    <Routes>
+                                        <Route path="/" element={<Home />} />
+                                        <Route path="/nosotros" element={<AboutUs />} />
+                                        <Route path="/contacto" element={<Contact />} />
+                                        <Route path="/products/:model" element={<ItemDetailContainer />} />
+                                    </Routes>
+                                </main>
+                                <Footer />
+                            </>
+                        }
+                    />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
+    );
 }
 
-export default App
+export default App;
